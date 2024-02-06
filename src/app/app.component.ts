636e47221c811fad34ae53139a25f58e61fa77b0
kamdem_arielle <arielle.kamdem@pether.io>
2024-02-06 22:51:02 +0100

    observables basic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, Subscriber } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'rxjs_project';
+export class AppComponent implements OnInit{
+
+
+  ngOnInit(){
+const observer={
+  next: (value:any) => console.log("next",value),
+  error: (value:any) => console.log("error",value),
+  complete:()=> console.log("completed!!")
+}
+
+    const observable=Observable.create((subscriber:any)=>{
+      subscriber.next("hello")
+      subscriber.next("world")
+      let count =0
+      const id = setInterval(()=>{
+        subscriber.next(count)
+        count++
+        if (count>5) {
+          subscriber.complete()
+        }
+      },1000)
+
+      
+    })
+
+
+  observable.subscribe(observer)
+  }
+
+  
+  
 }
