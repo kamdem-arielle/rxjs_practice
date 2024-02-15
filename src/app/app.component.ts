a82922c59544951836adb93d1ddf4c437fddd7ff
kamdem_arielle <arielle.kamdem@pether.io>
2024-02-15 23:08:57 +0100

    take op

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, Subscriber } from 'rxjs';
+import { AfterViewInit, Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Observable, Subscriber, fromEvent, map, of, take } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit,AfterViewInit
+{
+
+  constructor(private renderer:Renderer2){
+
+  }
+  ngAfterViewInit(): void {
+    const scrollbar = document.querySelector(".scroll")
+    this.progress.subscribe((data)=>{
+      console.log(data)
+      this.renderer.setStyle(scrollbar,"width",`${data}%`)
+    })
+  }
 
 
   ngOnInit(){
-const observer={
+  const observer={
   next: (value:any) => console.log("next",value),
   error: (value:any) => console.log("error",value),
   complete:()=> console.log("completed!!")
 }
 
+const number = of(1,2,3)
+number.pipe(
+  map((data)=>{
+    return data*2
+  }),
+  take(1)
+).subscribe(
+  (data)=>{
+    console.log(data)
+  },
+  ()=>{console.log("completed")}
+)
     const observable=Observable.create((subscriber:any)=>{
       subscriber.next("hello")
       subscriber.next("world")
@@ -33,8 +57,27 @@ const observer={
 
 
   observable.subscribe(observer)
+
   }
 
+
+
+   scroll = fromEvent(document,"scroll")
+   progress=this.scroll.pipe(
+    map((data:any)=>{
+     console.log(data)
+      return this.calculateScrollPercent(data.target.documentElement)
+    })
+   )
   
+ calculateScrollPercent(elem:any) {
+    const {
+      scrollTop,
+      scrollHeight,
+      clientHeight
+    }= elem
+    return (scrollTop/(scrollHeight - clientHeight))* 100
+
+  }
   
 }
